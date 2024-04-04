3fb95c0cb2b380737812228e4bab744868ad4fce
egor <sharapov.egor00@gmail.com>
2024-04-04 18:29:55 +0300

    Add admin data_statistics

--- data_statistics/admin.py
+++ data_statistics/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from data_statistics.models import Client, MailingStat
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email', 'comment')
+    search_fields = ('name', 'surname', 'email', 'comment')
+    ordering = ('email',)
+
+
+@admin.register(MailingStat)
+class MailingStatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status_attempt', 'attempt_datetime')
+    search_fields = ('name', 'attempt_datetime')
+    list_filter = ('name', 'status_attempt', 'response')
+    ordering = ('attempt_datetime',)
