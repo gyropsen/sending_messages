469ae363865137b817a1ae6eab4bfbbf130a4c47
egor <sharapov.egor00@gmail.com>
2024-05-14 18:16:14 +0300

    Continue config apscheduler, make it later

--- mailing/apps.py
+++ mailing/apps.py
@@ -5,6 +5,6 @@ class MailingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mailing"
 
-    def ready(self):
-        from mailing.services import start_scheduler
-        start_scheduler()
+    # def ready(self):
+    #     from mailing.services import start_scheduler
+    #     start_scheduler()
