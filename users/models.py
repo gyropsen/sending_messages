91dd3506e4d0e3439f62540d3a91405dcf05924d
egor <sharapov.egor00@gmail.com>
2024-05-14 18:37:26 +0300

    Create User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from mailing.models import NULLABLE
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name=_('email'))
+
+    name = models.CharField(max_length=64, verbose_name=_('name'))
+    surname = models.CharField(max_length=64, verbose_name=_('surname'))
+    avatar = models.ImageField(upload_to='users/avatars/', **NULLABLE, verbose_name=_('avatar'))
+    country = models.CharField(max_length=64, **NULLABLE, verbose_name=_('country'))
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+
+    def __str__(self):
+        return self.email
